864a17858284a529055da4c2d1c972d25dc70aa9
Kabir Sharma <sharmakabir204@gmail.com>
2025-04-29 11:46:07 +0530

    form html done

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,25 +1,45 @@
-import React, { useState } from 'react';
-import { UserData } from '../types';
+import React, { useState } from "react";
+import { UserData } from "../types";
 
 interface LoginProps {
-    onLoginSuccess : (userData: UserData) => void;
+  onLoginSuccess: (userData: UserData) => void;
 }
-const Login = ({ onLoginSuccess })=>{
-    const [rollNumber, setRollNumber] = useState('');
-    const [name, setName] = useState('');
-    const [error, setError] = useState('');
-    const [isLoading, setIsLoading]=useState(false);
-    
+const Login = ({ onLoginSuccess }) => {
+  const [rollNumber, setRollNumber] = useState("");
+  const [name, setName] = useState("");
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-
-
-    return(
-        <div className="login-container">
-            <h1>Login</h1>
-            <form onSubmit={handleSubmit}>
-                { if(error) && <div className='error-message'>{error}</div>}
-            </form>
-
+  return (
+    <div className="login-container">
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        {error && <div className="error-message">{error}</div>}
+        <div className="form-group">
+          <h1>Roll Number</h1>
+          <input
+            id="rollNumber"
+            type="text"
+            value={rollNumber}
+            onChange={(e) => setRollNumber(e.target.value)}
+            placeholder="roll no"
+          />
         </div>
-    )
-}
+        <div className="form-group">
+          <h1>Name</h1>
+          <input
+            id="name"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Enter your name"
+          />
+        </div>
+        <button type="submit">
+        {isLoading ? 'wait' : 'login'}
+        </button>
+      </form>
+    </div>
+  );
+};
+export default Login;
