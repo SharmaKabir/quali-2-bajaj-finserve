c1634ab861b1bffbb29e0ca5288ea918acab99b8
Kabir Sharma <sharmakabir204@gmail.com>
2025-04-29 11:38:48 +0530

    added login comp

---
+++ src/components/Login.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { UserData } from '../types';
+
+interface LoginProps {
+    onLoginSuccess : (userData: UserData) => void;
+}
+const Login = ({ onLoginSuccess })=>{
+    const [rollNumber, setRollNumber] = useState('');
+    const [name, setName] = useState('');
+    const [error, setError] = useState('');
+    const [isLoading, setIsLoading]=useState(tru);
+
+}
