5de789994d0a76a41d4ffa578bcf92cd3684099d
Kabir Sharma <sharmakabir204@gmail.com>
2025-04-29 11:31:46 +0530

    api fn added

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,3 +3,24 @@ import {FormResponse, UserData} from '../types';
 
 
 const API_BASE_URL ='https://dynamic-form-generator-9rl7.onrender.com';
+
+export const createUser = async (userData: UserData) => {
+    try {
+      const response = await axios.post(`${API_BASE_URL}/create-user`, userData);
+      return response.data;
+    } catch (error) {
+      console.log('createUser fn err', error);
+      throw error;
+    }
+};
+export const getFormStructure = async (rollNumber: string) => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/get-form`, {
+        params: { rollNumber }
+      });
+      return response.data;
+    } catch (error) {
+      console.log('getFormStruct err', error);
+      throw error;
+    }
+  };
