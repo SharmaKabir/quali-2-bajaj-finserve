3cd18960c0956751e361fd0a2999b65e5cd0d27f
Kabir Sharma <sharmakabir204@gmail.com>
2025-04-29 12:22:09 +0530

    added dynform for now

--- src/components/DynamicForm.tsx
+++ src/components/DynamicForm.tsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
+import { FormResponse, FormSection, FormValues } from '../types';
+import { DynamicField } from './FormFields';
 
-const DynamicForm = () => {
+
+interface DynamicFormProps {
+    formData: FormResponse;
+}
+
+
+
+
+const DynamicForm = (formData) => {
   return (
     <div>DynamicForm</div>
   )
